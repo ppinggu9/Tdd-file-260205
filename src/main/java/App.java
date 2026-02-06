958085986c7bd70c8b17177fb4f1c1c802f7a10d
ppinggu9 <pping5644@gmail.com>
2026-02-06 12:36:51 +0900

    명언 등록부터 명언증감까지 테스트

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    private Scanner scanner;
+    private Scanner sc;
+    int lastId = 0;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
-    public App(Scanner scanner){
-        this.scanner = scanner;
+
+    public App(Scanner sc) {
+        this.sc = sc;
     }
 
     public void run() {
-        System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
-        System.out.println("명언 : ");
-        System.out.println("작가 : ");
 
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.println("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                    lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+        }
     }
 }
--- src/test/java/AppTestRunner.java
+++ src/test/java/AppTestRunner.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class AppTestRunner {
 
     public static String run(String input) {
-        Scanner sc = TestUtil.genScanner(input); // 입력값
+        Scanner sc = TestUtil.genScanner(input + "\n종료");
 
         ByteArrayOutputStream outputStream = TestUtil.setOutByArray();// 배열로
         new App(sc).run(); // 실행
