55236bedb154d45803aa319ab8eccade4332f61c
ppinggu9 <pping5644@gmail.com>
2026-02-06 12:48:54 +0900

    controller 생성 및 주입된 sc 사용하기 위해 생성위치변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private Scanner sc;
-    int lastId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
-
+    private WiseSayingController wiseSayingController;
+    private SystemController systemController;
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -15,35 +12,19 @@ public class App {
 
     public void run() {
 
+        wiseSayingController = new WiseSayingController(sc); // 테스트에서 주입되는 sc을 사용하기 위해서
+        systemController = new SystemController();
+
         System.out.println("== 명언 앱 ==");
         while (true) {
             System.out.println("명령) ");
             String cmd = sc.nextLine();
 
             switch (cmd) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String saying = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-                    lastId++;
-                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
-                    wiseSayings.add(wiseSaying);
-                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
-
-                }
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
-                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
-
-                }
+                case "등록" -> wiseSayingController.actionAdd();
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
+                    systemController.actionExit();
                     return;
                 }
             }
